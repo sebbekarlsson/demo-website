0b83f63960ddd0ee79b4b545d8a3497b7723dfb0
ianertson <sebbekarlsson97@gmail.com>
2021-01-05 15:13:52 +0100

    added service file

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 virtualenv -p $(which python3.9) ./venv
 source ./venv/bin/activate
 ./venv/bin/python setup.py develop
-./venv/bin/python __main__.py
+
+cp ./demowebsite.service /etc/systemd/system/.
+systemctl daemon-reload
+systemctl restart demowebsite
